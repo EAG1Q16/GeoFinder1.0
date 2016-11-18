cdee227890ec44819fe71bdcaf75cb450e13f620
tonimsc <tonimxc@gmail.com>
2016-11-18 17:04:14 +0100

    Put de usuario

--- public/js/controllers/UserProfileCtrl.js
+++ public/js/controllers/UserProfileCtrl.js
@@ -3,15 +3,93 @@
  */
 angular.module('GeoFinderApp').controller('ProfileCtrl',['$scope','$rootScope','$location','$http','$routeParams', function($scope, $rootScope, $location, $http, $routeParams){
     console.log('Rootscope en profile' + $rootScope.UserSessionId);
+    $scope.UpdatedUser = {};
 
     // when landing on the page get user
    $http.get('/user/my/' + $rootScope.UserSessionId._id)
         .success(function(data) {
             $scope.UserProfileInfo = data;
+            console.log($scope.UserProfileInfo);
 
         })
         .error(function(data) {
             console.log('Error: ' + data);
         });
+
+   $scope.UpdateName = function() {
+       console.log('modificamos usuario');
+        console.log($scope.UpdatedUser.name);
+        $http.put('/user/update/name/' + $rootScope.UserSessionId._id, $scope.UpdatedUser)
+            .success(function(data){
+                console.log(data);
+                $scope.UserProfileInfo = data;
+                console.log($scope.UserProfileInfo);
+                $scope.UpdatedUser = {};
+            })
+            .error(function(data) {
+                console.log('Error' + data);
+                $scope.UpdatedUser = {};
+            });
+   };
+
+   $scope.UpdateUsername = function() {
+        console.log($scope.UpdatedUser);
+        $http.put('/user/update/username/' + $rootScope.UserSessionId._id, $scope.UpdatedUser)
+            .success(function(data){
+                console.log(data);
+                $scope.UserProfileInfo = data;
+                console.log($scope.UserProfileInfo);
+                $scope.UpdatedUser = {};
+            })
+            .error(function(data) {
+                console.log('Error' + data);
+                $scope.UpdatedUser = {};
+            });
+   };
+
+    $scope.UpdateDescription = function() {
+        console.log($scope.UpdatedUser.description);
+        $http.put('/user/update/description/' + $rootScope.UserSessionId._id, $scope.UpdatedUser)
+            .success(function(data){
+                console.log(data);
+                $scope.UserProfileInfo = data;
+                console.log($scope.UserProfileInfo);
+                $scope.UpdatedUser = {};
+            })
+            .error(function(data) {
+                console.log('Error' + data);
+                $scope.UpdatedUser = {};
+            });
+    };
+
+    $scope.UpdatePhoto = function() {
+        console.log($scope.UpdatedUser);
+        $http.put('/user/update/photo/' + $rootScope.UserSessionId._id, $scope.UpdatedUser)
+            .success(function(data){
+                console.log(data);
+                $scope.UserProfileInfo = data;
+                console.log($scope.UserProfileInfo);
+                $scope.UpdatedUser = {};
+            })
+            .error(function(data) {
+                console.log('Error' + data);
+                $scope.UpdatedUser = {};
+            });
+    };
+
+    $scope.UpdatePassword = function() {
+        console.log($scope.UpdatedUser);
+        $http.put('/user/update/password/' + $rootScope.UserSessionId._id, $scope.UpdatedUser)
+            .success(function(data){
+                console.log(data);
+                $scope.UserProfileInfo = data;
+                console.log($scope.UserProfileInfo);
+                $scope.UpdatedUser = {};
+            })
+            .error(function(data) {
+                console.log('Error' + data);
+                $scope.UpdatedUser = {};
+            });
+    };
 
 }]);
