d6fa671d74372660f2a40d177b6df26a8ae95f3d
tonimsc <tonimxc@gmail.com>
2016-11-23 15:15:32 +0100

    fixes after user model modified

--- public/js/controllers/PublicUserProfileCtrl.js
+++ public/js/controllers/PublicUserProfileCtrl.js
@@ -4,29 +4,27 @@
 angular.module('GeoFinderApp').controller('PublicUserProfileCtrl',['$scope','$rootScope','$window','$location','$http','$routeParams', function($scope, $rootScope, $window, $location, $http, $routeParams){
 
     var puserID = window.location.href.split("/").pop();
-    console.log(puserID);
-    console.log('rootscopeeee' + $rootScope.UserSessionId._id);
 
     // when landing on the page get adventure
     $http.get('/user/my/' + puserID)
         .success(function(data) {
             console.log(data);
             $scope.pUserProfileInfo = data;
-            $scope.pUserAdventures = data.adventures;
-            $scope.AdventuresLength = data.adventures.length;
+            $scope.CreatedAdventures = data.adventures.created;
+            $scope.AdventuresLength = data.adventures.created.length;
         })
         .error(function(data) {
             console.log('Error: ' + data);
         });
     // when landing on the page search if user is followed
-    $http.get('/user/isfollowing/' + puserID +'/'+ $rootScope.UserSessionId._id)
-        .success(function(data) {
-            console.log(data);
-            $scope.isfollowing = data;
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
-        });
+        $http.get('/user/isfollowing/' + puserID +'/'+ $rootScope.UserSessionUri)
+            .success(function(data) {
+                console.log(data);
+                $scope.isfollowing = data;
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
 
 
     $scope.followUser = function () {
